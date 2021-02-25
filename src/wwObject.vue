2410925335c8c94f91c66ebee7926ca47c0b62b6
Kévin Barfleur <kevinbarfleur@gmail.com>
2021-02-25 11:04:59 +0100

    first push

--- src/wwObject.vue
+++ src/wwObject.vue
@@ -1,12 +1,33 @@
 <template>
-    <div>Hello</div>
+    <div>
+        <wwObject
+            v-bind="content.button"
+            :wwProps="{}"
+            class="snipcart-add-item"
+            :data-item-id="content.id"
+            :data-item-price="content.price"
+            :data-item-description="content.description"
+            :data-item-image="content.image"
+            :data-item-name="content.name"
+        ></wwObject>
+    </div>
 </template>
 
 <script>
 export default {
-    name: '__COMPONENT_NAME__',
+    props: {
+        content: { type: Object, required: true },
+    },
+    wwDefaultContent: {
+        button: wwLib.element({ type: 'ww-button', content: { text: { en: 'Add to cart' } } }),
+        itemId: 'mr-toucan',
+        name: 'Mr. Toucan',
+        price: '99.99',
+        description:
+            'The toco toucan (Ramphastos toco), also known as the common toucan or giant toucan, is the largest and probably the best known species in the toucan family. It is found in semi-open habitats throughout a large part of central and eastern South America. It is a common attraction in zoos.',
+        image: null,
+    },
 };
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
