27f69a461ce7b18ad47be86f09cecd05bbab0c29
Kévin Barfleur <kevinbarfleur@gmail.com>
2021-05-04 15:36:27 +0200

    fix custom props

--- src/wwObject.vue
+++ src/wwObject.vue
@@ -1,29 +1,30 @@
 <template>
-    <div @mouseenter="log">
-        <wwObject
-            ref="el"
-            v-bind="content.button"
-            :wwProps="{}"
-            class="snipcart-add-item"
-            :data-item-id="content.id"
-            :data-item-url="content.url"
-            :data-item-price="content.price"
-            :data-item-description="content.description"
-            :data-item-image="content.image"
-            :data-item-name="content.name"
-            data-item-custom1-name="Frame color"
-            data-item-custom1-options="Black|Brown|Gold"
-            :data-customProps="content.customProps"
-        >
-            ></wwObject
-        >
-    </div>
+    <wwObject
+        ref="el"
+        v-bind="content.button"
+        :wwProps="{}"
+        class="snipcart-add-item"
+        :data-item-id="content.id"
+        :data-item-url="content.url"
+        :data-item-price="content.price"
+        :data-item-description="content.description"
+        :data-item-image="content.image"
+        :data-item-name="content.name"
+        data-item-custom1-name="Frame color"
+        data-item-custom1-options="Black|Brown|Gold"
+        :data-customProps="customProps"
+    >
+        ></wwObject
+    >
 </template>
 
 <script>
 export default {
     props: {
         content: { type: Object, required: true },
+        /* wwEditor:start */
+        wwEditorState: Object,
+        /* wwEditor:end */
     },
     wwDefaultContent: {
         button: wwLib.element({ type: 'ww-button', content: { text: { en: 'Add to cart' } } }),
@@ -35,26 +36,31 @@ export default {
             'The toco toucan (Ramphastos toco), also known as the common toucan or giant toucan, is the largest and probably the best known species in the toucan family. It is found in semi-open habitats throughout a large part of central and eastern South America. It is a common attraction in zoos.',
         image: null,
         customProps: [],
+        globalCurrency: 'usd',
+    },
+    watch: {
+        'content.customProps'() {
+            this.customProps = JSON.parse(this.content.customProps);
+        },
+    },
+    computed: {
+        isEditing() {
+            /* wwEditor:start */
+            return this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION;
+            /* wwEditor:end */
+            // eslint-disable-next-line no-unreachable
+            return false;
+        },
     },
     data() {
         return {
-            customProps: null,
+            customProps: '',
         };
     },
-    methods: {
-        log() {
-            const props = JSON.parse(
-                this.$store.getters['cms/getData']['eb21514a-761f-49d2-b56d-c1a1a396e8a5'].data[0].props
-            );
-            this.customProps = props;
-            // for (const prop in props) {
-            //     this.addAttribute(prop, props[prop]);
-            // }
-        },
-        // addAttribute(attr, value) {
-        //     console.log(attr, value);
-        //     console.log(this.$refs.el);
-        // },
+    mounted() {
+        if (this.content.globalCurrency && !this.isEditing) {
+            window.Snipcart.api.session.setCurrency(this.content.globalCurrency);
+        }
     },
 };
 </script>
