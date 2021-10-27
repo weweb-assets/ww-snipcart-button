f65838e7a5613139aeedee80a571217feb292294
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2021-10-27 19:30:25 +0200

    Fix snipcart error / Update with new array field (#1)
    * webapp
    
    * webapp
    
    * removed wwLib.element
    
    * Fox ww-config
    
    * Fix and icon
    
    * div fix
    
    * default button content
    
    * Element bubble
    
    * test
    
    * remove usless class
    
    * fix verif
    
    * update ww-config
    
    * add snipcart class
    
    * test witch latest cli
    
    * update properties computed dans remove bind for property name
    
    Co-authored-by: Marc Fabre <nds.kram@gmail.com>

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,57 +1,113 @@
 <template>
-    <wwElement
-        v-bind="content.button"
-        :ww-props="{}"
-        class="snipcart-add-item"
+    <div
+        class="ww-snipcart-button snipcart-add-item"
         :data-item-id="content.id"
         :data-item-url="content.url"
         :data-item-price="content.price"
         :data-item-description="content.description"
         :data-item-image="content.image"
         :data-item-name="content.name"
+        v-bind="properties"
+        :class="{ editing: isEditing, selected: isSelected }"
     >
-    </wwElement>
+        <wwElement v-bind="content.button"></wwElement>
+        <!-- wwEditor:start -->
+        <div class="ww-snipcart-button__menu">
+            <wwEditorIcon small name="fontawesome/solid/shopping-cart" />
+        </div>
+        <!-- wwEditor:end -->
+    </div>
 </template>
 
 <script>
 export default {
     props: {
+        /* wwEditor:start */
+        wwEditorState: { type: Object, required: true },
+        /* wwEditor:end */
         content: { type: Object, required: true },
     },
-    wwDefaultContent: {
-        button: wwLib.element({ type: 'ww-button', content: { text: { en: 'Add to cart' } } }),
-        itemId: 'mr-toucan',
-        url: '/MrToucan',
-        name: 'Mr. Toucan',
-        price: '99.99',
-        description:
-            'The toco toucan (Ramphastos toco), also known as the common toucan or giant toucan, is the largest and probably the best known species in the toucan family. It is found in semi-open habitats throughout a large part of central and eastern South America. It is a common attraction in zoos.',
-        image: null,
-        customProps: '',
-    },
-    watch: {
-        'content.customProps'() {
-            this.addAttribute();
+    computed: {
+        isEditing() {
+            /* wwEditor:start */
+            return this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION;
+            /* wwEditor:end */
+            // eslint-disable-next-line no-unreachable
+            return false;
         },
-    },
-    mounted() {
-        this.addAttribute();
-    },
-    methods: {
-        addAttribute() {
-            if (!this.content.customProps || !this.content.customProps.length) return;
-            let props = this.content.customProps.split('data-item');
-            props.shift();
-            for (let item of props) {
-                item = 'data-item' + item;
-                const prop = item.split(':');
-                const attr = prop[0] ? prop[0].trim() : '';
-                const value = prop[1] ? prop[1].trim() : '';
-                
-                if(this.$el.setAttribute) this.$el.setAttribute(attr, value);
-                else if(this.$el.nextElementSibling) this.$el.nextElementSibling.setAttribute(attr, value);                
+        isSelected() {
+            /* wwEditor:start */
+            return this.wwEditorState.isSelected;
+            /* wwEditor:end */
+            // eslint-disable-next-line no-unreachable
+            return false;
+        },
+        properties() {
+            if (!this.content.customProps) return {};
+            let props = {};
+
+            for (let item of this.content.customProps) {
+                if (item && item.property) {
+                    props[item.property] = item.value || '';
+                }
             }
+
+            return props;
         },
     },
 };
 </script>
+
+<style lang="scss" scoped>
+.ww-snipcart-button {
+    /* wwEditor:start */
+    &.selected {
+        > .ww-snipcart-button__status {
+            opacity: 1;
+            pointer-events: all;
+        }
+    }
+    &.editing:hover {
+        & > .border {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            border: 1px solid var(--ww-editor-color);
+            pointer-events: none;
+            z-index: 10;
+        }
+        > .ww-snipcart-button__menu {
+            opacity: 1;
+            pointer-events: all;
+        }
+    }
+    &__menu {
+        display: flex;
+        position: absolute;
+        top: 0px;
+        left: 5px;
+        transform: translate(-50%, -50%);
+        border-radius: 100%;
+        padding: var(--ww-spacing-01);
+        transition: opacity 0.2s ease;
+        z-index: 101;
+        cursor: pointer;
+        background-color: var(--ww-color-blue-500);
+        color: var(--ww-color-white);
+        justify-content: center;
+        align-items: center;
+        opacity: 0;
+        pointer-events: none;
+        &:after {
+            content: '';
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%) rotate(45deg);
+        }
+    }
+    /* wwEditor:end */
+}
+</style>
---
+++ ww-config.js
@@ -0,0 +1,101 @@
+export default {
+    editor: {
+        label: {
+            en: 'Snipcart button',
+        },
+        icon: 'fontawesome/solid/shopping-cart',
+    },
+    properties: {
+        button: {
+            hidden: true,
+            defaultValue: { isWwObject: true, type: 'ww-button', content: { text: { en: 'Add to cart' } } },
+        },
+        id: {
+            label: {
+                en: 'Id',
+            },
+            options: {
+                placeholder: 'ID',
+            },
+            type: 'Text',
+            bindable: true,
+            section: 'settings',
+            defaultValue: 'mr-toucan',
+        },
+        url: {
+            label: {
+                en: 'Url',
+            },
+            options: {
+                placeholder: 'url',
+            },
+            type: 'Text',
+            bindable: true,
+            section: 'settings',
+            defaultValue: '/MrToucan',
+        },
+        name: {
+            label: {
+                en: 'Name',
+            },
+            type: 'Textarea',
+            bindable: true,
+            section: 'settings',
+            defaultValue: 'Mr. Toucan',
+        },
+        price: {
+            label: {
+                en: 'Price',
+            },
+            type: 'Text',
+            bindable: true,
+            section: 'settings',
+            defaultValue: '99.99',
+        },
+        description: {
+            label: {
+                en: 'Description',
+            },
+            type: 'Textarea',
+            bindable: true,
+            section: 'settings',
+            defaultValue:
+                'The toco toucan (Ramphastos toco), also known as the common toucan or giant toucan, is the largest and probably the best known species in the toucan family. It is found in semi-open habitats throughout a large part of central and eastern South America. It is a common attraction in zoos.',
+        },
+        image: {
+            label: {
+                en: 'Snipcart image',
+            },
+            type: 'Image',
+            bindable: true,
+            section: 'settings',
+            defaultValue: null,
+        },
+        customProps: {
+            label: { en: 'Custom props', fr: 'Custom props' },
+            section: 'settings',
+            bindable: true,
+            type: 'Array',
+            options: {
+                item: {
+                    type: 'Object',
+                    options: {
+                        item: {
+                            property: {
+                                type: 'Text',
+                                options: { placeholder: 'Property' },
+                                defaultValue: '',
+                            },
+                            value: {
+                                type: 'Text',
+                                options: { placeholder: 'Value' },
+                                bindable: true,
+                                defaultValue: '',
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    },
+};
